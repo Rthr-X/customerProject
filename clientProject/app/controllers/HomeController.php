2c6ecfd24e9982e5556528778deba0fc510a019c
Rthr-X <Steven Parradee>
2014-05-21 16:28:51 -0500

    Figuring out some Route/Controller interactions

--- clientProject/app/controllers/HomeController.php
+++ clientProject/app/controllers/HomeController.php
@@ -4,15 +4,18 @@ class HomeController extends BaseController {
 
 	public function index($route="home") {
 
-        Debugbar::addMessage("Opening route '{$route}'");
+        Debugbar::addMessage("HomeController::index - Opening route '{$route}'");
 
         switch($route) {
             case "about":
-               return View::make('about', []);
-
+               return View::make('about');
+                break;
+            case "/":
             case "home":
+                return View::make('home');
+                break;
             default:
-                return View::make('home', []);
+                return View::make('error404', ['url'=>$route]);
                 break;
         }
 
--- clientProject/app/routes.php
+++ clientProject/app/routes.php
@@ -1,22 +1,4 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
-
-#Route::get('/', function() {
-#	return View::make('homePage');
-#});
-
-Route::get('/{route?}', ['as'=>'home', 'uses'=>'homeController@index']);
-
-
-// Final catch-all routine, throw to a 404 page
-Route::any('{all}', function($page) { return View::make('error404'); })->where('all', '.*');
+// A universal URL parser, throwing it to the HomeController
+Route::any('{all}', ['uses'=>'HomeController@index'])->where('all', '.*');
