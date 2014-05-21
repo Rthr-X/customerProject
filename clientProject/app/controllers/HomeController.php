b9dc1087cad74599bdb2c4fbe7f6e5fc559619e0
Rthr-X <Steven Parradee>
2014-05-21 17:17:00 -0500

    Collin's route fixes!  Undid most of my work!  huzzah!

--- clientProject/app/controllers/HomeController.php
+++ clientProject/app/controllers/HomeController.php
@@ -2,23 +2,24 @@
 
 class HomeController extends BaseController {
 
-	public function index($route="home") {
+    public function missingMethod($params = []) {
 
-        Debugbar::addMessage("HomeController::index - Opening route '{$route}'");
+        Debugbar::addMessage("HomeController::index - Opening route '{$params[0]}'");
 
-        switch($route) {
+        switch($params[0]) {
             case "about":
-               return View::make('about');
+                return View::make('about');
                 break;
             case "/":
             case "home":
                 return View::make('home');
                 break;
             default:
-                return View::make('error404', ['url'=>$route]);
+                return View::make('error404', ['url'=>$params[0]]);
                 break;
         }
 
-	}
+#        dd($params);
+    }
 
 }
--- clientProject/app/routes.php
+++ clientProject/app/routes.php
@@ -1,4 +1,7 @@
 <?php
 
 // A universal URL parser, throwing it to the HomeController
-Route::any('{all}', ['uses'=>'HomeController@index'])->where('all', '.*');
+
+//Route::any('{all}', ['uses'=>'HomeController@index'])->where('all', '.*');
+
+Route::controller('/', 'HomeController');
