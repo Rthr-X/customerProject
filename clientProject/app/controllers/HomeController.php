382f56e20fd235ca5bd587667b5db63ce1cb5dab
Rthr-X <Steven Parradee>
2014-05-21 15:05:15 -0500

    Removed extraneous debugbarcode

--- clientProject/app/controllers/HomeController.php
+++ clientProject/app/controllers/HomeController.php
@@ -4,13 +4,7 @@ class HomeController extends BaseController {
 
 	public function index() {
 
-        $debugbar = new DebugBar\StandardDebugBar();
-        $debugbarRenderer = $debugbar->getJavascriptRenderer();
-
-		return View::make('home', [
-            'debugBarHeader' => $debugbarRenderer->renderHead(),
-            'debugBarScripts' => $debugbarRenderer->render(),
-        ]);
+		return View::make('home', []);
 
 	}
 
